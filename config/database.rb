2ef9819131155bfae6f0c88643f6b4ef6b56938b
Nick Yee <njsyee@gmail.com>
2016-01-27 20:22:00 -0700

    Changed db to postgres.

--- config/database.rb
+++ config/database.rb
@@ -4,10 +4,19 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
+  # set :database, {
+  #   adapter: "postgresql",
+  #   database: "db/development"
+  # }
+
+  ActiveRecord::Base.establish_connection(
+    adapter: "postgresql",
+    host: "ec2-54-225-197-143.compute-1.amazonaws.com",
+    database: "d55mvpk1jcmgfl",
+    user: "syixxfnlmtnqjr",
+    password: "P8S_eujkGCve2Ro1KfisURtoiM",
+    post: "5432"
+  )
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
